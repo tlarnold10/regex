0fe0a78da26402e54e41a9abd4c9a2f8d75fa36c
tlarnold10 <tlarnold10@hotmail.com>
2020-10-21 21:04:21 -0500

    added a new findall

--- regex.py
+++ regex.py
@@ -1,6 +1,7 @@
 import re
 
 def main():
+    # great place to learn regex: https://www.computerhope.com/jargon/r/regex.htm
     old_string = "hello kendra, you are a bad ass bitch. Syd loves you so much"
 
     # Matches any characters between 'a' and 'z'
@@ -13,6 +14,14 @@ def main():
 
     # Matches anything not contained in brackets
     new_string = re.sub('[^a-zA-z ]', 'sooooo', old_string)
+
+    print(old_string)
+    print(new_string)
+
+    old_string = 'I am lost in Paris... this is not fun'
+
+    # Matching any word that starts with a capital
+    new_string = re.findall('[A-Z]\w+', old_string)[0]
 
     print(old_string)
     print(new_string)
